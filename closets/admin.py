e4beeea3c441c582716150bf640806fbd410b0ac
ericak11 <kantor.erica@gmail.com>
2015-01-09 14:58:30 -0500

    added owner model to extend user model

--- closets/admin.py
+++ closets/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from closets.models import Owner
 # Register your models here.
+
+class OwnerInline(admin.StackedInline):
+    model = Owner
+    can_delete = False
+    verbose_name_plural = 'owner'
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (OwnerInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- closets/models.py
+++ closets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Owner(models.Model):
+    user = models.OneToOneField(User)
+    GENDER = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+        ('O', 'Other'),
+    )
+    gender = models.CharField(max_length=10, choices=GENDER)

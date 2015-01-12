138b0a66538c25b28f0a743aa0e85d6a2c4697ef
ericak11 <kantor.erica@gmail.com>
2015-01-12 12:21:47 -0500

    updated admin file to include new tables

--- closets/admin.py
+++ closets/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from closets.models import Owner
+from closets.models import Closet
+from closets.models import Item
 # Register your models here.
 
 class OwnerInline(admin.StackedInline):
@@ -16,3 +18,5 @@ class UserAdmin(UserAdmin):
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Closet)
+admin.site.register(Item)

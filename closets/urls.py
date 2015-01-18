3d7ba276ba5ec1a757e31ce221a2a2a1c77ef924
ericak11 <kantor.erica@gmail.com>
2015-01-18 17:16:53 -0500

    added owner list page

--- closets/urls.py
+++ closets/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from closets import views
+from closets.views import OwnerList
 
 urlpatterns = patterns('',
-      url(r'^$', views.user_login, name='user_login'),
+      # url(r'^$', views.user_login, name='user_login'),
+      url(r'^$', OwnerList.as_view()),
 )
--- closets/views.py
+++ closets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from closets.models import Owner, Closet, Tag, Item
 # Create your views here.
+
+class OwnerList(ListView):
+    model = Owner
+    template_name = 'owner/list.html'
